494426f4bb5c1f300a14f3e8b3c4ee82a54b02c9
Victor <victorjongsoon@hotmail.com>
2026-05-22 21:25:27 +0800

    producer with key

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoKeys.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoKeys.java
@@ -1,9 +1,6 @@
 package io.conduktor.demos.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,66 +11,65 @@ public class ProducerDemoKeys {
 
     private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
 
-    public static void main(String[] args) {
-        log.info("I am a Kafka Producer!");
+    public static void main(String[] args)
+    {
+        log.info("Starting Producer...");
 
         // create Producer Properties
         Properties properties = new Properties();
 
         // connect to Localhost
-//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // connect to Conduktor Playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
+//        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
 
         // set producer properties
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
         // create the Producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-
-        for (int j=0; j<2; j++){
-
-            for (int i=0; i<10; i++){
+        for (int j = 0; j < 2; j++){
+            for (int i = 0; i < 10; i++){
 
                 String topic = "demo_java";
                 String key = "id_" + i;
                 String value = "hello world " + i;
 
                 // create a Producer Record
-                ProducerRecord<String, String> producerRecord =
-                        new ProducerRecord<>(topic, key, value);
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(topic, key, value);
 
                 // send data
                 producer.send(producerRecord, new Callback() {
                     @Override
-                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
                         // executes every time a record successfully sent or an exception is thrown
-                        if (e == null) {
+                        if (exception == null){
                             // the record was successfully sent
                             log.info("Key: " + key + " | Partition: " + metadata.partition());
-                        } else {
-                            log.error("Error while producing", e);
+                        }
+                        else {
+                            log.error("Error while producing", exception);
                         }
                     }
                 });
-            }
-
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
 
 
 
-        // tell the producer to send all data and block until done -- synchronous
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // tell the producer to send all data and block until done - synchronous
         producer.flush();
 
         // flush and close the producer
