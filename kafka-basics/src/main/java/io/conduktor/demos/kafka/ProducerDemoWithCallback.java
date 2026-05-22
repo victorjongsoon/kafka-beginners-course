3a1b2b3d4773e9409585568015db2f036c396fed
Victor <victorjongsoon@hotmail.com>
2026-05-22 21:15:49 +0800

    producer with callback

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -1,9 +1,6 @@
 package io.conduktor.demos.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,70 +11,73 @@ public class ProducerDemoWithCallback {
 
     private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
 
-    public static void main(String[] args) {
-        log.info("I am a Kafka Producer!");
+    public static void main(String[] args)
+    {
+        log.info("Starting Producer...");
 
         // create Producer Properties
         Properties properties = new Properties();
 
         // connect to Localhost
-//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // connect to Conduktor Playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
+//        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
 
         // set producer properties
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
-        properties.setProperty("batch.size", "400");
+        // not recommended in prod, default is 16384
+         properties.setProperty("batch.size", "400");
 
-//        properties.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
+        // not recommended in prod
+        /* default is based on key
+         * 1. If the record has a key
+         * same key -> same partition
+         * 2. If the record has no key
+         * Kafka uses the default sticky partitioning behavior:
+         * send multiple records to one partition for a while,
+         * then switch to another partition
+         * */
+        properties.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
 
         // create the Producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
+        // produce 300 records
+        for (int i = 0; i < 300; i++){
+            // create a Producer Record
+            final int messageNumber = i;
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("demo_java", "hello world: " + messageNumber);
 
-        for (int j=0; j<10; j++){
-
-            for (int i=0; i<30; i++){
-
-                // create a Producer Record
-                ProducerRecord<String, String> producerRecord =
-                        new ProducerRecord<>("demo_java", "hello world " + i);
-
-                // send data
-                producer.send(producerRecord, new Callback() {
-                    @Override
-                    public void onCompletion(RecordMetadata metadata, Exception e) {
-                        // executes every time a record successfully sent or an exception is thrown
-                        if (e == null) {
-                            // the record was successfully sent
-                            log.info("Received new metadata \n" +
-                                    "Topic: " + metadata.topic() + "\n" +
-                                    "Partition: " + metadata.partition() + "\n" +
-                                    "Offset: " + metadata.offset() + "\n" +
-                                    "Timestamp: " + metadata.timestamp());
-                        } else {
-                            log.error("Error while producing", e);
-                        }
+            // send data
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    // executes every time a record successfully sent or an exception is thrown
+                    if (exception == null){
+                        // the record was successfully sent
+                        log.info("Message {} -> partition {} offset {}", messageNumber, metadata.partition(), metadata.offset());
                     }
-                });
-            }
-
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
+                    else {
+                        log.error("Error while producing", exception);
+                    }
+                }
+            });
         }
 
 
-        // tell the producer to send all data and block until done -- synchronous
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // tell the producer to send all data and block until done - synchronous
         producer.flush();
 
         // flush and close the producer
