7a57f3a1492f631601774054653d0779a0b009fa
Victor <victorjongsoon@hotmail.com>
2026-05-23 11:48:08 +0800

    consumer

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -3,7 +3,10 @@ package io.conduktor.demos.kafka;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,8 +18,9 @@ public class ConsumerDemo {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
 
-    public static void main(String[] args) {
-        log.info("I am a Kafka Consumer!");
+    public static void main(String[] args)
+    {
+        log.info("Starting Consumer...");
 
         String groupId = "my-java-application";
         String topic = "demo_java";
@@ -25,42 +29,43 @@ public class ConsumerDemo {
         Properties properties = new Properties();
 
         // connect to Localhost
-//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // connect to Conduktor Playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
+//        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
 
         // create consumer configs
         properties.setProperty("key.deserializer", StringDeserializer.class.getName());
         properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
         properties.setProperty("group.id", groupId);
+
+        /*
+        * auto.offset.reset controls what a consumer does when there is no committed offset for its consumer group, or when the committed offset is no longer valid.
+        * none: Throw an error if there is no committed offset.
+        * earliest: Start reading from the oldest available message in the topic partition.
+        * latest: Start reading only from new messages produced after the consumer starts.
+        * */
         properties.setProperty("auto.offset.reset", "earliest");
 
         // create a consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
         // subscribe to a topic
         consumer.subscribe(Arrays.asList(topic));
 
         // poll for data
         while (true) {
-
             log.info("Polling");
-
-            ConsumerRecords<String, String> records =
-                    consumer.poll(Duration.ofMillis(1000));
-
-            for (ConsumerRecord<String, String> record: records) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records)
+            {
                 log.info("Key: " + record.key() + ", Value: " + record.value());
                 log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
             }
-
-
         }
-
-
     }
 }
