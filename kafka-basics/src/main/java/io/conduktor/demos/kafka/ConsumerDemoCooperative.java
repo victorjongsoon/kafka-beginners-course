20d76a3d1a6ce7945add73106e4193580e8a994d
Victor <victorjongsoon@hotmail.com>
2026-05-23 13:17:43 +0800

    consumer cooperative

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemoCooperative.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemoCooperative.java
@@ -17,8 +17,9 @@ public class ConsumerDemoCooperative {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class.getSimpleName());
 
-    public static void main(String[] args) {
-        log.info("I am a Kafka Consumer!");
+    public static void main(String[] args)
+    {
+        log.info("Starting Consumer...");
 
         String groupId = "my-java-application";
         String topic = "demo_java";
@@ -27,25 +28,34 @@ public class ConsumerDemoCooperative {
         Properties properties = new Properties();
 
         // connect to Localhost
-//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // connect to Conduktor Playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
+//        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
 
         // create consumer configs
         properties.setProperty("key.deserializer", StringDeserializer.class.getName());
         properties.setProperty("value.deserializer", StringDeserializer.class.getName());
-        properties.setProperty("group.id", groupId);
-        properties.setProperty("auto.offset.reset", "earliest");
-        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
-//        properties.setProperty("group.instance.id", "...."); // strategy for static assignments
 
+        properties.setProperty("group.id", groupId);
+
+        /*
+         * auto.offset.reset controls what a consumer does when there is no committed offset for its consumer group, or when the committed offset is no longer valid.
+         * none: Throw an error if there is no committed offset.
+         * earliest: Start reading from the oldest available message in the topic partition.
+         * latest: Start reading only from new messages produced after the consumer starts.
+         * */
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+
+        // properties.setProperty("group.instance.id", ""); // strategy for static assignments
 
         // create a consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
         // get a reference to the main thread
         final Thread mainThread = Thread.currentThread();
@@ -60,38 +70,32 @@ public class ConsumerDemoCooperative {
                 try {
                     mainThread.join();
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
             }
         });
 
-
-
         try {
             // subscribe to a topic
             consumer.subscribe(Arrays.asList(topic));
+
             // poll for data
             while (true) {
-
-                ConsumerRecords<String, String> records =
-                        consumer.poll(Duration.ofMillis(1000));
-
-                for (ConsumerRecord<String, String> record : records) {
+                log.info("Polling");
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records)
+                {
                     log.info("Key: " + record.key() + ", Value: " + record.value());
                     log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
                 }
-
             }
-
         } catch (WakeupException e) {
-            log.info("Consumer is starting to shut down");
+            log.info("Consumer is starting to shutdown");
         } catch (Exception e) {
             log.error("Unexpected exception in the consumer", e);
         } finally {
-            consumer.close(); // close the consumer, this will also commit offsets
-            log.info("The consumer is now gracefully shut down");
+            consumer.close(); // close the consumer, this will also commit the offset
+            log.info("The consumer is now gracefully shutdown");
         }
-
-
     }
 }
