4f5fd2e5b24fe825837fa27181f7d08cc32d5677
Victor <victorjongsoon@hotmail.com>
2026-05-24 14:42:42 +0800

    manually handle offsets

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -87,6 +87,10 @@ public class OpenSearchConsumer {
                     }
 
                 }
+
+                // commit offsets after the batch is consumed
+                consumer.commitSync();
+                log.info("Offsets have been committed.");
             }
         }
 
@@ -146,6 +150,7 @@ public class OpenSearchConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         // create consumer
         return new KafkaConsumer<>(properties);
